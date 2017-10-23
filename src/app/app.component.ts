44865b5430c9c7619fcac7a550699360139570db
Olaide Agboola <olaideagboola@Olaides-MacBook-Pro.local>
2017-10-23 22:53:14 +0100

    not yet ubeatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,20 +17,20 @@ export class AppComponent {
   public playRows = [];
   public playCols = []
   public playIndexes = [];
-  public diagonal1 = ['0,0', '1,1', '2,2'];
-  public diagonal2 = ['0,2', '1,1', '2,0'];
+  public diagonal1 = [];
+  public diagonal2 = [];
   public moves = 0;
-  public corners = ['0,0', '0,2', '2,0', '2,2'];
-  public middle = ['1,1'];
 
   constructor(){}
 
   userPlay(i, j) {
-    this.tictac[i][j] = 'X';
-    this.moves++;
-    this.recordPlay(i, j, 'X');
-    if(this.moves != 9) {
-      this.randomPlay();
+    if(this.tictac[i][j] == ' ') {
+      this.tictac[i][j] = 'X';
+      this.moves++;
+      this.recordPlay(i, j, 'X');
+      if(this.moves != 9) {
+        this.randomPlay();
+      }
     }
   }
 
@@ -50,20 +50,65 @@ export class AppComponent {
     }
   }
 
-  checkforWin(){
+  checkforWin(i, j){
+    if ( this.playRows[i].length == 3 && !this.playRows[i].includes(undefined)) {
+      return this.determineWinner(this.playRows[i])
+    }
+    if( this.playCols[j].length === 3 && !this.playCols[j].includes(undefined)) {
+      return this.determineWinner(this.playCols[j])
+    }
+    if (this.diagonal1.length == 3 && !this.diagonal1.includes(undefined)) {
+      return this.determineWinner(this.diagonal1)
+    }
+    if (this.diagonal2.length == 3 && !this.diagonal2.includes(undefined)) {
+      return this.determineWinner(this.diagonal2)
+    }
   }
 
-  recordPlay(i, j, play) {j
-    console.log('i and j', i, j)
+  determineWinner(collection){
+    if(collection.indexOf('X') == -1) {
+      alert('Player O wins the game');
+      this.reset();
+    } else if(collection.indexOf('O') == -1) {
+      alert('Player X wins the game');
+      this.reset();
+    }
+  }
+
+  //check if a row, column or diagonal is complete
+
+  recordPlay(i, j, play) {
     if(!this.playRows[i]){
       this.playRows[i] = [];
     }
     if(!this.playCols[j]) {
       this.playCols[j] = [];
     }
+    if(i == j) {
+      this.diagonal1.push(play)
+    }
+    if((i == 0 && j == 2) || (i == 2 && j == 0) || (i == 1 && j ==1)){
+      this.diagonal2.push(play);
+    }
     this.playRows[i][j] = play;
     this.playCols[j][i] = play;
-    this.playIndexes.push(i + ',' +j)
+    this.playIndexes.push(i + ',' +j);
+    this.checkforWin(i, j);
+  }
+
+  reset(){
+    this.tictac =
+    [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
+    ];
+    this.playRows = [];
+    this.playCols = []
+    this.playIndexes = [];
+    this.diagonal1 = [];
+    this.diagonal2 = [];
+    this.moves = 0;
   }
 }
 
